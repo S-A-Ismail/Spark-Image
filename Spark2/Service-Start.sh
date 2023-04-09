618b01022805c3c1bdbc85837696c3b318a8661f
S.A.Ismail <syedahmedismail98@gmail.com>
2023-04-09 16:31:23 -0700

    Changes

--- Spark2/Service-Start.sh
+++ Spark2/Service-Start.sh
@@ -4,7 +4,8 @@
 /mnt/spark/bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
   
 # Start the second process
-jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
+export SPARK_HOME="/mnt/spark"
+/mnt/livy/bin/livy-server start
   
 # Wait for any process to exit
 wait -n
